9955d7bd5938585fcf1b4cc45c1c554e6f47e383
David Alfonso <davidalfonso565@gmail.com>
2024-10-10 02:11:37 -0600

    añadimos aplicacion

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-st.header('Lanzar una moneda')
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
+st.header('Proyecto 6')
+st.write('Eliija el diagrama que desea crear')
+
+hist_checkbox = st.checkbox('Construir histograma') # crear un botón       
+if hist_checkbox: # al hacer clic en el checkbox
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+    
+scatter_checkbox= st.checkbox('contruir un scatter')
+if scatter_checkbox: # al hacer clic en el checkbox
+    # escribir un mensaje
+    st.write('Creación de un scatter para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer", y= 'price')
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
